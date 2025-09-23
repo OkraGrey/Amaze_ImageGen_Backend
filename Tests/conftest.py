3fdc893d24b38ff65f6437ffcb65b73606a85cef
okragrey <hasnainsohail15@gmail.com>
2025-09-23 18:54:42 +0500

    Fix Env issues

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -4,7 +4,6 @@ import pytest
 import sys
 import pathlib
 
-# Ensure project root is on PYTHONPATH
 PROJECT_ROOT = pathlib.Path(__file__).resolve().parents[1]
 if str(PROJECT_ROOT) not in sys.path:
     sys.path.insert(0, str(PROJECT_ROOT))
